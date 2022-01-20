50c0bc08ffb5c8b172cba5c007d5f5c93b8f578c
Daniel Granger <dg1234uk@me.com>
2022-01-20 16:22:39 +0000

    Set up files for vercel

--- app/post.ts
+++ app/post.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import parseFrontMatter from 'front-matter';
-import { readFileSync, readdirSync } from 'fs';
+import { readFileSync, readdirSync, writeFileSync } from 'fs';
 import invariant from 'tiny-invariant';
 import { marked } from 'marked';
 
@@ -13,7 +13,7 @@ export type PostMarkdownAttributes = {
   title: string;
 };
 
-const postsPath = path.join(__dirname, '../posts');
+const postsPath = path.join(__dirname, '../../posts');
 
 function isValidPostAttributes(
   attributes: any
@@ -27,13 +27,13 @@ type NewPost = {
   markdown: string;
 };
 
-// export async function createPost(post: NewPost) {
-//   const md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
-//   await writeFile(path.join(postsPath, post.slug + '.md'), md);
-//   return getPost(post.slug);
-// }
+export function createPost(post: NewPost) {
+  const md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
+  writeFileSync(path.join(postsPath, post.slug + '.md'), md);
+  return getPost(post.slug);
+}
 
-export async function getPost(slug: string) {
+export function getPost(slug: string) {
   const filepath = path.join(postsPath, slug + '.md');
   const file = readFileSync(filepath);
   const { attributes, body } = parseFrontMatter(file.toString());
@@ -45,10 +45,10 @@ export async function getPost(slug: string) {
   return { slug, html, title: attributes.title };
 }
 
-export async function getPosts() {
-  const dir = await readdirSync(postsPath);
+export function getPosts() {
+  const dir = readdirSync(postsPath);
   return Promise.all(
-    dir.map(async (filename) => {
+    dir.map((filename) => {
       const file = readFileSync(path.join(postsPath, filename));
       const { attributes } = parseFrontMatter(file.toString());
       invariant(isValidPostAttributes(attributes));
